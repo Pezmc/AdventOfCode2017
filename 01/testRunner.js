e76e06b646778a8620951600acec37a3529d25f0
Pez Cuckow <email@pezcuckow.com>
2017-12-01 10:49:41 +0000

    Day 1: Inverse Captcha

---
+++ 01/testRunner.js
@@ -0,0 +1,28 @@
+module.exports = class TestRunner {
+	constructor(testCases, puzzleInput, solver) {
+    this.testCases = testCases;
+    this.puzzleInput = puzzleInput;
+    this.solver = solver;
+	}
+
+	runTests() {
+		this.testCases.forEach((testCase, i) => {
+			const output = this.solver(testCase.input);
+			if (output == testCase.expected) {
+				console.log(`✓ Test case ${i} passed`);
+			} else {
+				console.error(`✗ Test case ${i} failed`);
+				console.info(`Expected: "${testCase.expected}", actual: "${testCase.actual}"`);
+			}
+		});
+	}
+
+  solve() {
+    console.log(`✌ The answer to the puzzle is: "${this.solver(this.puzzleInput)}"`);
+  }
+
+  run() {
+    this.runTests();
+    this.solve();
+  }
+}
